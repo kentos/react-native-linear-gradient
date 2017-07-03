09c75e504c8613f6b67321a060a1926b630004ae
kentos <kent@kentcederstrom.se>
2017-07-03 10:37:58 +0200

    Use ViewPropTypes

--- index.android.js
+++ index.android.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component, PropTypes, ViewPropTypes } from 'react';
 import { processColor, requireNativeComponent, PointPropType, StyleSheet, View } from 'react-native';
 const deprecatedPropType = require('react-native/Libraries/Utilities/deprecatedPropType.js');
 
@@ -42,7 +42,7 @@ export default class LinearGradient extends Component {
     ]),
     colors: PropTypes.arrayOf(PropTypes.string).isRequired,
     locations: PropTypes.arrayOf(PropTypes.number),
-    ...View.propTypes,
+    ...ViewPropTypes,
   };
   props: PropsType;
   gradientRef: any;
